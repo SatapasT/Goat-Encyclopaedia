309207cd42576fbe084c249d87bc7ca53b1b5fd0
Satapas T <satapastanachotpaisit@gmail.com>
2023-12-28 19:03:28 +0700

    added a navbar

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(express.static('client'));
 
-
-let list = ["yes", "no"];
+let list = ["yes", "no", "test"];
 
 app.post('/getData', (req, resp) => {
     resp.send(list);
 });
 
-app.get('/list', function (req, resp){
+app.get('/list', function (req, resp) {
     resp.send(list);
 });
 
