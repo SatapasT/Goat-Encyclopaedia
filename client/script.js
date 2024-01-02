56ee06c8aef24c229442c2a1ddcbd8f771949ed7
Satapas T <satapastanachotpaisit@gmail.com>
2024-01-02 23:02:02 +0700

    Added function to all the nav pills

--- client/script.js
+++ client/script.js
@@ -1,25 +1,46 @@
+document.getElementById('kiko_goat').addEventListener('click', fetchData_kiko);
 document.getElementById('pygora_goat').addEventListener('click', fetchData_pygora);
+document.getElementById('angora_goat').addEventListener('click', fetchData_angora);
+document.getElementById('pygmy_goat').addEventListener('click', fetchData_pygmy);
 
+
+let species = ""
 
 function fetchData_pygora() {
-    console.log("GOATS?");
-    fetch('http://127.0.0.1:8080/pygora') 
-        .then(response => response.text())  
-        .then(data => {
-            document.getElementById('result').innerHTML = data;
-            console.log("GOATS?");
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
-    console.log("GOATS?");
-    fetch('http://127.0.0.1:8080/pygora/name') 
-        .then(response => response.text())  
-        .then(data => {
-            document.getElementById('display_center').innerHTML = data;
-            console.log("GOATS?");
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
+    species = 'pygora_goat';
+    updatePage();
+}
+
+function fetchData_kiko() {
+    species = 'kiko_goat';
+    updatePage();
+}
+
+function fetchData_angora() {
+    species = 'angora_goat';
+    updatePage();
+}
+
+function fetchData_pygmy() {
+    species = 'pygmy_goat';
+    updatePage();
+}
+
+function updatePage() {
+    fetch(`http://127.0.0.1:8080/${species}`)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('display_center').innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+fetch(`http://127.0.0.1:8080/goat_name/${species}`)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('display_center').innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 }
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
+app.use(express.static('client'));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
@@ -8,30 +10,23 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(express.static('client'));
+app.use(express.json());
+const json_file = 'data/data.json';
+const data = JSON.parse(fs.readFileSync(json_file));
 
-let list = ["yes", "no", "test"];
-let pygora_goat = { Name: "Pygora Goat" };
-
-app.post('/getData', (req, resp) => {
-    resp.send(list);
+app.get('/goat_name/:species', (req, resp) => {
+    const species = req.params.species;
+    const goatEntry = data.find(entry => entry.species.includes(species));
+    if (goatEntry) {
+        resp.send(goatEntry.name[0]);
+        console.log(species, goatEntry);
+    } else {
+        resp.status(404).send('Loading error, try again');
+        console.log('loading error');
+    }
 });
 
-app.post('/pygora', (req, resp) => {
-    resp.send(pygora_goat);
-});
 
-app.get('/pygora/name', (req, resp) => {
-    resp.send(pygora_goat["Name"]);
-});
-
-app.get('/pygora', (req, resp) => {
-    resp.send(pygora_goat);
-});
-
-app.get('/list', function (req, resp) {
-    resp.send(pygora_goat);
-});
 
 const server = app.listen(8080, () => {
     console.log(`Server is running on port ${server.address().port}`);
